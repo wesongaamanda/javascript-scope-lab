6253e927667266bb157c0892bbd74f621cf38ea6
wesongaamanda <wesongaamanda@gmail.com>
2026-04-09 13:24:36 +0300

    add

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 // Write your solution in this file!
 const burgers= [ 'Hamburger', 'Cheeseburger'];
-   let featuredDrink = 'Strawberry Milkshake';
+let featuredDrink = 'Strawberry Milkshake';
 
-   function addBurger () {
+
+function addBurger () {
     const newBurger = 'Flatburger';
     burgers.push(newBurger);
-   }
+}
+
 
 if (true) {
    const anotherNewBurger ='Maple Bacon Burger';
    burgers.push(anotherNewBurger);
 }
 
+
 function changeFeaturedDrink() {
-   let featuredDrink = 'The JavaShake';
+   featuredDrink = 'The JavaShake';
 }
