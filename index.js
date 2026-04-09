30af527cd7f1fc417fa9fbd7aea25fd467af3d4a
wesongaamanda <wesongaamanda@gmail.com>
2026-04-09 13:13:51 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+const burgers= [ 'Hamburger', 'Cheeseburger'];
+   let featuredDrink = 'Strawberry Milkshake';
+
+   function addBurger () {
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger);
+   }
+
+if (true) {
+   const anotherNewBurger ='Maple Bacon Burger';
+   burgers.push(anotherNewBurger);
+}
+
+function changeFeaturedDrink() {
+   let featuredDrink = 'The JavaShake';
+}
